486372949ce39978f3771267213a35058ab6dbe8
naumanmallik <naumanmalik.dev@gmail.com>
2022-02-16 10:59:52 +0500

    handle data of menu

---
+++ src/common/CreateMenuDialog.js
@@ -0,0 +1,87 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+  Grid,
+} from "@mui/material";
+
+import { useForm, Controller } from "react-hook-form";
+
+const CreateMenuDialog = ({ open, handleClose }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const createMenu = (data) => {
+    console.log(data);
+    handleClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+      <form onSubmit={handleSubmit(createMenu)} autoComplete="off">
+        <DialogTitle>Create New Menu</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2} direction="column" sx={{ mt: 1 }}>
+            <Grid item>
+              <Controller
+                name="name"
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    variant="outlined"
+                    helperText={
+                      errors?.name?.type === "required" &&
+                      "This field is required"
+                    }
+                    error={errors?.name?.type === "required"}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item>
+              <Controller
+                name="description"
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    id="description"
+                    label="Description"
+                    variant="outlined"
+                    helperText={
+                      errors?.description?.type === "required" &&
+                      "This field is required"
+                    }
+                    error={errors?.description?.type === "required"}
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+};
+
+export default CreateMenuDialog;
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,18 +1,27 @@
+import * as React from "react";
 import { Button, Container } from "@mui/material";
+import CreateMenuDialog from "../../common/CreateMenuDialog";
 import classes from "./Dashboard.module.css";
 import MenuCard from "./MenuCard";
 
 const Dashboard = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpenDialog = () => setOpen((prev) => !prev);
+
   return (
     <>
       <Container maxWidth="lg" className={classes.btnRoot}>
-        <Button variant="outlined">Create New Menu</Button>
+        <Button variant="outlined" onClick={handleOpenDialog}>
+          Create New Menu
+        </Button>
       </Container>
       <div className={classes.container}>
         {[1, 2, 3, 4, 5, 6, 7].map((val, index) => (
           <MenuCard index={index} />
         ))}
       </div>
+      <CreateMenuDialog open={open} handleClose={handleOpenDialog} />
     </>
   );
 };
